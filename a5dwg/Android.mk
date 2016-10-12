9ac7b4d30dbce2f40adf70c7ddbb44cafdbce953
bigsupersquid <0.uradork.0@gmail.com>
2016-10-12 17:26:20 -0500

    badly described commit again
    Change-Id: Ifd30d9fabde55417b6a6eeaac01137552c863494

--- a5dwg/Android.mk
+++ a5dwg/Android.mk
@@ -19,13 +19,13 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter a5dwg,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libril
-LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/lib/libril.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_PREBUILT)
+#LOCAL_MODULE := libril
+#LOCAL_MODULE_OWNER := htc
+#LOCAL_SRC_FILES := proprietary/lib/libril.so
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_SUFFIX := .so
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#include $(BUILD_PREBUILT)
 
 endif
 
